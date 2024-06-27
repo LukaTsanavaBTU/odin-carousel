86cccaac7bc09cc295fe8273515c5ab633a2a5fb
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-06-27 12:41:13 +0400

    Add page selection, finished product without animations

--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ const carouselHandler = (function () {
   const images = document.querySelectorAll(".image-div img");
   const leftArrow = document.querySelector(".left-arrow");
   const rightArrow = document.querySelector(".right-arrow");
+  const orderButtonsWrapper = document.querySelector(".order-buttons-wrapper");
 
   let currentImageIndex = 0;
   let imageCount = images.length;
+
+  for (let i = 0; i < imageCount; i++) {
+    const orderButton = document.createElement("div");
+    orderButton.textContent = "●";
+    orderButton.classList.add("order-button");
+    if (i === 0) {
+      orderButton.classList.add("selected");
+    }
+    orderButtonsWrapper.appendChild(orderButton);
+  }
+
+  const orderButtons = document.querySelectorAll(".order-button");
+
+  const setOrderSelected = function (selectedIndex) {
+    orderButtons[selectedIndex].classList.add("selected");
+    orderButtons.forEach((button, index) => {
+      if (index !== selectedIndex) {
+        button.classList.remove("selected");
+      }
+    });
+  };
 
   const setActive = function setImageAsActiveAndDisableOthers(index) {
     images[index].classList.add("active");
@@ -15,7 +37,14 @@ const carouselHandler = (function () {
         img.classList.remove("active");
       }
     });
+    setOrderSelected(index);
   };
+
+  orderButtons.forEach((clickedButton, clickedIndex) => {
+    clickedButton.addEventListener("click", (e) => {
+      setActive.call(e.target, clickedIndex);
+    });
+  });
 
   leftArrow.addEventListener("click", () => {
     const targetImageIndex =
